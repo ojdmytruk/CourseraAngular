6f907eb90b09adbc8c378e38a478329e60c6c680
ojdmytruk <oj.dmytruk@gmail.com>
2020-07-09 17:21:21 +0300

    Single Page Applications Part 2

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
 import { from } from 'rxjs';
+import { MAX_LENGTH_VALIDATOR } from '@angular/forms/src/directives/validators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -8,13 +12,21 @@ import { from } from 'rxjs';
   styleUrls: ['./dishdetail.component.scss']
 })
 
-export class DishdetailComponent implements OnInit
-{
-  @Input()
+export class DishdetailComponent implements OnInit {
+
   dish: Dish;
-  constructor() { }
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id.toString());
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
